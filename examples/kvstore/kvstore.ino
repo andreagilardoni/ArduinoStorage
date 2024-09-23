91040029439500723e4557b14a2c09577f99a59c
Andrea Gilardoni <a.gilardoni@arduino.cc>
2024-09-23 15:48:02 +0200

    fixup! adding example for kvstore

--- examples/kvstore/kvstore.ino
+++ examples/kvstore/kvstore.ino
@@ -1,17 +1,19 @@
 #include <Arduino_KVStore.h>
 
+// REDIRECT_STDOUT_TO(Serial);
 
 void setup() {
     Serial.begin(115200);
 
     while(!Serial);
     delay(5000);
-    KVStore kvstore("pippo");
+    KVStore kvstore;
 
     kvstore.begin();
 
     uint32_t value = 0x01020304;
 
+    kvstore.remove("0");
     Serial.println(kvstore.putUInt("0", value));
     Serial.println(kvstore.getUInt("0"), HEX);
     kvstore.remove("0");
@@ -19,6 +21,7 @@ void setup() {
     Serial.println(kvstore.putBytes("0", (uint8_t*)&value, sizeof(value)));
     // Serial.println(kvstore.getbytes("0"), HEX);
     kvstore.remove("0");
+    Serial.println(kvstore.exists("4"));
 }
 
 void loop() {
