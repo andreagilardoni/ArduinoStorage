7d847b9b4398118261a618906cdea4a62198551a
Andrea Gilardoni <a.gilardoni@arduino.cc>
2025-03-19 10:54:43 +0100

    Revert "[TMP] added delete_value callback and destructor to reference"
    This reverts commit df405c271b8693376b10e3d9cc0c9df2dcbae546.

--- src/kvstore/kvstore.h
+++ src/kvstore/kvstore.h
@@ -38,14 +38,7 @@ public:
     template<typename T>
     class reference {
     public:
-        reference(const key_t &key, T value, KVStoreInterface& owner, std::function<void(T)> delete_value = nullptr)
-        : key(key), value(value), delete_value(delete_value), owner(owner) {}
-
-        ~reference() {
-            if(delete_value) {
-                delete_value(value);
-            }
-        }
+        reference(const key_t &key, T value, KVStoreInterface& owner): key(key), value(value), owner(owner) {}
 
         // assign a new value to the reference and update the store
         reference& operator=(T t) noexcept {
@@ -84,7 +77,6 @@ public:
     private:
         const key_t key;
         T value;
-        const std::function<void(T)> delete_value;
 
         KVStoreInterface<Key>& owner;
     };
