72a56b7503f812be0b1524080b54bb790861d915
Andrea Gilardoni <a.gilardoni@arduino.cc>
2025-03-20 15:01:56 +0100

    fixup! enabling kvstore for rp2040

--- src/Arduino_KVStore.h
+++ src/Arduino_KVStore.h
@@ -1,10 +1,19 @@
+/*
+ * This file is part of Arduino_KVStore.
+ *
+ * Copyright (c) 2024 Arduino SA
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #pragma once
 #include "kvstore/kvstore.h"
 
 #if defined(ARDUINO_UNOR4_WIFI)
 #include "kvstore/implementation/UnoR4.h"
 
-// using KVStore = Unor4KVStore<const char*>;
 using KVStore = Unor4KVStore;
 
 #elif defined(ARDUINO_PORTENTA_H7_M7) || defined(ARDUINO_NICLA_VISION) \
@@ -18,12 +27,19 @@ using KVStore = STM32H7KVStore;
 #include "kvstore/implementation/portentac33.h"
 
 using KVStore = PortentaC33KVStore;
+
 #elif defined(ARDUINO_ARCH_SAMD) || defined(ARDUINO_NANO_RP2040_CONNECT)
 
 #include "kvstore/implementation/SAMD.h"
 
 using KVStore = SAMDKVStore;
 
+#elif defined(ARDUINO_ARCH_ESP32)
+
+#include "kvstore/implementation/ESP32.h"
+
+using KVStore = ESP32KVStore;
+
 #else
-#error "KVStore not present on current platform"
+#error "Arduino KVStore is not supported on current platform"
 #endif
